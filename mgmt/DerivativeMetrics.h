4a1fb8c6a3cd83507045a9a54f967ae692101d3b
Leif Hedstrom <zwoop@apache.org>
2018-05-18 19:41:15 -0600

    Replaces the old metrics.config with a simple C++ class
    This retains the ~10 metrics that were calculated before in Lua (and XML
    before that), which are actually useful. I've not examined what other
    derivative metrics be useful to add, but this is a good start. Adding new
    metrics to this "system" is easy, but for now it only supports sum() of
    process metrics into a new derivative metric.
    
    Eventually I imagine this will be part of traffic_server itself, which is
    why I put this in mgmt/ rather than in cmd/traffic_manager, where the old
    Lua code resided.

---
+++ mgmt/DerivativeMetrics.h
@@ -0,0 +1,44 @@
+/** @file
+
+  Calculate some derivative metrics (for convenience).
+
+  @section license License
+
+  Licensed to the Apache Software Foundation (ASF) under one
+  or more contributor license agreements.  See the NOTICE file
+  distributed with this work for additional information
+  regarding copyright ownership.  The ASF licenses this file
+  to you under the Apache License, Version 2.0 (the
+  "License"); you may not use this file except in compliance
+  with the License.  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+
+#pragma once
+
+#include <tuple>
+#include "I_RecLocal.h"
+
+// ToDo: It's a little bizarre that we include this here, but it's the only way to get to RecSetRecord(). We should
+// move that elsewhere... But other places in our core does the same thing.
+#include "P_RecCore.h"
+
+using DerivativeSum = std::tuple<const char *, RecDataT, std::vector<const char *>>;
+
+class DerivativeMetrics
+{
+public:
+  DerivativeMetrics();
+  void Update();
+
+  // Don't allow copy and assign
+  DerivativeMetrics(DerivativeMetrics &) = delete;
+  DerivativeMetrics &operator=(DerivativeMetrics &) = delete;
+};
