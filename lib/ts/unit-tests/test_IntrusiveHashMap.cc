8054d78e9e93f656015f705ac5af979868b0735e
Alan M. Carroll <amc@apache.org>
2018-08-28 17:18:43 -0500

    IntrusiveHashMap: Performance tests.

--- lib/ts/unit-tests/test_IntrusiveHashMap.cc
+++ lib/ts/unit-tests/test_IntrusiveHashMap.cc
@@ -25,6 +25,10 @@
 #include <string_view>
 #include <string>
 #include <bitset>
+#include <vector>
+#include <unordered_map>
+#include <chrono>
+#include <random>
 #include <ts/IntrusiveHashMap.h>
 #include <ts/BufferWriter.h>
 #include <catch.hpp>
@@ -146,3 +150,209 @@ TEST_CASE("IntrusiveHashMap", "[libts][IntrusiveHashMap]")
   // clean up the last bits.
   map.apply([](Thing *thing) { delete thing; });
 };
+
+// Normally there's no point in running the performance tests, but it's worth keeping the code
+// for when additional testing needs to be done.
+#if 0
+
+struct Thing2 {
+  std::string _payload;
+  int _n{0};
+
+  Thing2(std::string_view text) : _payload(text) {}
+  Thing2(std::string_view text, int x) : _payload(text), _n(x) {}
+
+  Thing2 * _next_name {nullptr};
+  Thing2 * _prev_name {nullptr};
+
+  struct LinkByName {
+    static Thing2*&
+    next_ptr(Thing2 *thing)
+    {
+      return thing->_next_name;
+    }
+    static Thing2 *&
+    prev_ptr(Thing2 *thing)
+    {
+      return thing->_prev_name;
+    }
+    static std::string_view
+    key_of(Thing2 *thing)
+    {
+      return thing->_payload;
+    }
+    static constexpr std::hash<std::string_view> hasher{};
+    static auto
+    hash_of(std::string_view s) -> decltype(hasher(s))
+    {
+      return hasher(s);
+    }
+    static bool
+    equal(std::string_view const &lhs, std::string_view const &rhs)
+    {
+      return lhs == rhs;
+    }
+  };
+
+  Thing2 *_next_n{nullptr};
+  Thing2 *_prev_n{nullptr};
+
+  struct LinkByN {
+    static Thing2*&
+    next_ptr(Thing2 *thing)
+    {
+      return thing->_next_n;
+    }
+    static Thing2 *&
+    prev_ptr(Thing2 *thing)
+    {
+      return thing->_prev_n;
+    }
+    static int
+    key_of(Thing2 *thing)
+    {
+      return thing->_n;
+    }
+    static int
+    hash_of(int n)
+    {
+      return n;
+    }
+    static bool
+    equal(int lhs, int rhs)
+    {
+      return lhs == rhs;
+    }
+  };
+};
+
+TEST_CASE("IntrusiveHashMapPerf", "[IntrusiveHashMap][performance]")
+{
+  // Force these so I can easily change the set of tests.
+  auto start            = std::chrono::high_resolution_clock::now();
+  auto delta = std::chrono::high_resolution_clock::now() - start;
+  constexpr int N_LOOPS = 1000;
+  constexpr int N = 1009; // prime > N_LOOPS
+
+  std::vector<const char *> strings;
+
+  std::uniform_int_distribution<short> char_gen {'a', 'z'};
+  std::uniform_int_distribution<short> length_gen { 20, 40 };
+  std::minstd_rand randu;
+
+  Map ihm;
+  std::unordered_map<std::string, Thing> um;
+
+  strings.reserve(N);
+  for ( int i = 0 ; i < N ; ++i ) {
+    auto len = length_gen(randu);
+    char *s = static_cast<char *>(malloc(len + 1));
+    for (decltype(len) j = 0; j < len; ++j) {
+      s[j] = char_gen(randu);
+    }
+    s[len] = 0;
+    strings.push_back(s);
+  }
+
+  // Fill the IntrusiveHashMap
+  start = std::chrono::high_resolution_clock::now();
+  for (int i = 0; i < N ; ++i) {
+    auto thing = new Thing{strings[i], i};
+    ihm.insert(thing);
+  }
+  delta = std::chrono::high_resolution_clock::now() - start;
+  std::cout << "IHM populate " << delta.count() << "ns or " << std::chrono::duration_cast<std::chrono::milliseconds>(delta).count()
+            << "ms" << std::endl;
+
+  // Do some lookups
+  start = std::chrono::high_resolution_clock::now();
+  for ( int i = 0 ; i < N_LOOPS ; ++i ) {
+    for (int j = 0, idx = i; j < N; ++j, idx = (idx + i) % N) {
+      ihm.find(strings[idx]);
+    }
+  }
+  delta = std::chrono::high_resolution_clock::now() - start;
+  std::cout << "IHM find " << delta.count() << "ns or " << std::chrono::duration_cast<std::chrono::milliseconds>(delta).count()
+            << "ms" << std::endl;
+
+  // Fill the std::unordered_map
+  start = std::chrono::high_resolution_clock::now();
+  for (int i = 0; i < N ; ++i) {
+    um.emplace(strings[i], Thing{strings[i], i});
+  }
+  delta = std::chrono::high_resolution_clock::now() - start;
+  std::cout << "UM populate " << delta.count() << "ns or " << std::chrono::duration_cast<std::chrono::milliseconds>(delta).count()
+            << "ms" << std::endl;
+
+  // Do some lookups
+  start = std::chrono::high_resolution_clock::now();
+  for ( int i = 0 ; i < N_LOOPS ; ++i ) {
+    for (int j = 0, idx = i; j < N; ++j, idx = (idx + i) % N) {
+      um.find(strings[idx]);
+    }
+  }
+  delta = std::chrono::high_resolution_clock::now() - start;
+  std::cout << "UM find " << delta.count() << "ns or " << std::chrono::duration_cast<std::chrono::milliseconds>(delta).count()
+            << "ms" << std::endl;
+
+  // Test double indexing
+
+  std::vector<Thing2> things;
+  things.reserve(N);
+  for ( int i = 0 ; i < N ; ++i ) {
+    things.emplace_back(strings[i], i);
+  }
+
+  std::unordered_map<std::string, Thing2*> um_by_name;
+  std::unordered_map<int, Thing2*> um_by_n;
+
+  IntrusiveHashMap<Thing2::LinkByName> ihm_by_name;
+  IntrusiveHashMap<Thing2::LinkByN> ihm_by_n;
+
+  // Fill the IntrusiveHashMap
+  start = std::chrono::high_resolution_clock::now();
+  for (int i = 0; i < N ; ++i) {
+    ihm_by_name.insert(&things[i]);
+    ihm_by_n.insert(&things[i]);
+  }
+  delta = std::chrono::high_resolution_clock::now() - start;
+  std::cout << "IHM2 populate " << delta.count() << "ns or " << std::chrono::duration_cast<std::chrono::milliseconds>(delta).count()
+            << "ms" << std::endl;
+
+  // Do some lookups
+  start = std::chrono::high_resolution_clock::now();
+  for ( int i = 0 ; i < N_LOOPS ; ++i ) {
+    for (int j = 0, idx = i; j < N; ++j, idx = (idx + i) % N) {
+      Thing2 * thing = ihm_by_n.find(idx);
+      ihm_by_name.iterator_for(thing);
+    }
+  }
+  delta = std::chrono::high_resolution_clock::now() - start;
+  std::cout << "IHM2 find " << delta.count() << "ns or " << std::chrono::duration_cast<std::chrono::milliseconds>(delta).count()
+            << "ms" << std::endl;
+
+  // Fill the std::unordered_map
+  start = std::chrono::high_resolution_clock::now();
+  for (int i = 0; i < N ; ++i) {
+    um_by_n.emplace(things[i]._n, &things[i]);
+    um_by_name.emplace(things[i]._payload, &things[i]);
+  }
+  delta = std::chrono::high_resolution_clock::now() - start;
+  std::cout << "UM2 populate " << delta.count() << "ns or " << std::chrono::duration_cast<std::chrono::milliseconds>(delta).count()
+            << "ms" << std::endl;
+
+  // Do some lookups
+  start = std::chrono::high_resolution_clock::now();
+  for ( int i = 0 ; i < N_LOOPS ; ++i ) {
+    for (int j = 0, idx = i; j < N; ++j, idx = (idx + i) % N) {
+      um_by_name.find(strings[idx]);
+      um_by_n.find(idx);
+    }
+  }
+  delta = std::chrono::high_resolution_clock::now() - start;
+  std::cout << "UM2 find " << delta.count() << "ns or " << std::chrono::duration_cast<std::chrono::milliseconds>(delta).count()
+            << "ms" << std::endl;
+
+
+};
+#endif
