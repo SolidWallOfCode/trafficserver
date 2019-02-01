9b50a3d6e90fdcd693c8c1d84360b398757a5c0d
Oknet Xu <xuchao@skyguard.com.cn>
2019-02-01 18:25:54 +0800

    Avoid rescheduling HostDBContinuation to the ET_DNS thread.
    With TS-196 (commit a5345da8), the HostDBContinuation is no longer
    reschedule to the ET_DNS. However, the changes only applied to the
    `HostDBProcessor::getbyname_imm` interface.
    
    And the NCA feature was removed by TS-455 (commit 112b03aa), so it is
    safe to bring the changes to all the interfaces of HostDBProcessor.
    
    (cherry picked from commit 76e50f0ea46923b39fc3a408826b8be6959fe799)

--- iocore/hostdb/HostDB.cc
+++ iocore/hostdb/HostDB.cc
@@ -672,16 +672,7 @@ Lretry:
   c->init(md5, opt);
   SET_CONTINUATION_HANDLER(c, (HostDBContHandler)&HostDBContinuation::probeEvent);
 
-  // Since ProxyMutexPtr has a cast operator, gcc-3.x get upset
-  // about ambiguity when doing this comparison, so by reversing
-  // the operands, I force it to pick the cast operation /leif.
-  if (thread->mutex == cont->mutex) {
-    thread->schedule_in(c, MUTEX_RETRY_DELAY);
-  } else {
-    // Make sure the affinity thread is set before casting off to dns thread
-    c->setThreadAffinity(thread);
-    dnsProcessor.thread->schedule_imm(c);
-  }
+  thread->schedule_in(c, MUTEX_RETRY_DELAY);
 
   return &c->action;
 }
@@ -787,11 +778,7 @@ HostDBProcessor::getSRVbyname_imm(Continuation *cont, process_srv_info_pfn proce
   c->init(md5, copt);
   SET_CONTINUATION_HANDLER(c, (HostDBContHandler)&HostDBContinuation::probeEvent);
 
-  if (thread->mutex == cont->mutex) {
-    thread->schedule_in(c, MUTEX_RETRY_DELAY);
-  } else {
-    dnsProcessor.thread->schedule_imm(c);
-  }
+  thread->schedule_in(c, MUTEX_RETRY_DELAY);
 
   return &c->action;
 }
@@ -889,11 +876,7 @@ HostDBProcessor::iterate(Continuation *cont)
   c->current_iterate_pos = 0;
   SET_CONTINUATION_HANDLER(c, (HostDBContHandler)&HostDBContinuation::iterateEvent);
 
-  if (thread->mutex == cont->mutex) {
-    thread->schedule_in(c, HOST_DB_RETRY_PERIOD);
-  } else {
-    dnsProcessor.thread->schedule_imm(c);
-  }
+  thread->schedule_in(c, HOST_DB_RETRY_PERIOD);
 
   return &c->action;
 }
