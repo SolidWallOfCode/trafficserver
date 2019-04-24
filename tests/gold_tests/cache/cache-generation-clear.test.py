222acc04376935f5c2156b19584d6671e1ccd9dd
Walter Karas <wkaras@verizonmedia.com>
2019-04-23 19:23:46 -0500

    Restore skipping on Au test cache-generation-clear.
    It does not work reliably due to YTSATS-2686.

--- tests/gold_tests/cache/cache-generation-clear.test.py
+++ tests/gold_tests/cache/cache-generation-clear.test.py
@@ -21,8 +21,9 @@ import uuid
 Test.Summary = '''
 Test that incrementing the cache generation acts like a cache clear
 '''
+Test.SkipUnless(Condition.InEnvAllowList("cache-generation-clear", "Skipped due to YTSATS-2686"),
 # need Curl
-Test.SkipUnless(Condition.HasProgram("curl", "Curl need to be installed on system for this test to work"))
+                Condition.HasProgram("curl", "Curl need to be installed on system for this test to work"))
 Test.ContinueOnFail = True
 # Define default ATS
 ts = Test.MakeATSProcess("ts", command="traffic_manager")
